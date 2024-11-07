c4b201d88ed25267e389ab4ea9cdb7374244dac3
abelb407 <164031783+abelb407@users.noreply.github.com>
2024-11-07 15:58:58 +0000

    Storing questions in a separate file

--- TheProject/Context/DatabaseSeeder.cs
+++ TheProject/Context/DatabaseSeeder.cs
@@ -45,6 +45,25 @@ namespace TheProject.Context
                 await _userManager.AddToRoleAsync(admin, "Admin");
 
             }
+
+
+            if (!_context.Questions.Any())
+            {
+                var questions = GetQuestions();
+                _context.Questions.AddRange(questions);
+                await _context.SaveChangesAsync();
+            }
+
+        }
+        private List<Questions> GetQuestions()
+        {
+            return new List<Questions>
+    {
+        new Questions { Question = "Which group is always found in an alcohol?", Topic = "Alcohols", Option = new List<string> { "Hydroxyl", "Carbonyl", "Nitrile", "Phosphate" }, Answer = "Hydroxyl" },
+        new Questions { Question = "Which one is organic?", Topic = "Alkanes", Option = new List<string> { "Tin", "Carbon", "Lead", "Platinum" }, Answer = "Carbon" },
+        new Questions { Question = "Who's the electrophile?", Topic = "Alkenes", Option = new List<string> { "Ammonia", "Hydroxide Ion", "Hydronium Ion", "Cyanide Ion" }, Answer = "Hydronium Ion" },
+        new Questions { Question = "Which one is aminey?", Topic = "Amines", Option = new List<string> { "Bomb", "Nitrogen", "Propanone", "Sulfur" }, Answer = "Nitrogen" }
+        };
         }
 
 
